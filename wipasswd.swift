b87bbb6d9c91bd01057c4d5003d5cce471ce1beb
Masayoshi Sekimura <sekimura@gmail.com>
2015-02-17 12:18:02 -0800

    style: more cleanups

--- wipasswd.swift
+++ wipasswd.swift
@@ -8,7 +8,6 @@ import Foundation
 let kSecClassValue = kSecClass as NSString
 let kSecAttrServiceValue = kSecAttrService as NSString
 let kSecAttrAccountValue = kSecAttrAccount as NSString
-let kSecValueDataValue = kSecValueData as NSString
 let kSecClassGenericPasswordValue = kSecClassGenericPassword as NSString
 let kSecMatchLimitValue = kSecMatchLimit as NSString
 let kSecReturnDataValue = kSecReturnData as NSString
@@ -16,7 +15,7 @@ let kSecMatchLimitOneValue = kSecMatchLimitOne as NSString
 let kAirPortService = "AirPort"
 
 
-func get_passwd(userAccount : String) -> String? { 
+func getPasswd(userAccount : String) -> String? { 
     var keychainQuery: NSMutableDictionary = NSMutableDictionary(
         objects: [kSecClassGenericPasswordValue, kAirPortService, userAccount, kCFBooleanTrue, kSecMatchLimitOneValue],
         forKeys: [kSecClassValue, kSecAttrServiceValue, kSecAttrAccountValue, kSecReturnDataValue, kSecMatchLimitValue])
@@ -25,13 +24,10 @@ func get_passwd(userAccount : String) -> String? {
 
     let status: OSStatus = SecItemCopyMatching(keychainQuery, &dataTypeRef)
 
-
     let opaque = dataTypeRef?.toOpaque()
     if let op = opaque? {
         let retrievedData = Unmanaged<NSData>.fromOpaque(op).takeUnretainedValue()
-
-        // Convert the data retrieved from the keychain into a string
-        return NSString(data: retrievedData, encoding: NSUTF8StringEncoding) as NSString?
+        return NSString(data:retrievedData, encoding:NSUTF8StringEncoding) as NSString?
     } else {
         return nil
     }
@@ -52,10 +48,10 @@ func main() {
     }
 
     let ssid = args[1]
-    let contentsOfKeychain = get_passwd(ssid)
-    if let passwd = contentsOfKeychain? {
+    let contentsOfKeychain = getPasswd(ssid)
+    if let pass = contentsOfKeychain? {
         println("SSID: \(ssid)")
-        println("PASS: \(passwd)")
+        println("PASS: \(pass)")
     } else {
         println("No WiFi password found for \(ssid)")
     }
